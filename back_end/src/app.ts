323427e145707b8f62f5f5d915561cf481e915e4
Italo Araujo <italojsouza@gmail.com>
2024-02-19 14:27:40 -0300

    testing react-script library

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import emailController from './controllers/email.controller';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //  Rota unica 
 
