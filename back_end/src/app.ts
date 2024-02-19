ecf91da5f910aa2fb630d4fed0adac85f424f96d
Italo Araujo <italojsouza@gmail.com>
2024-02-19 11:55:24 -0300

    add comments

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import emailController from './controllers/email.controller';
 
 const app = express();
-
 app.use(express.json());
+
+//  Rota unica 
+
 app.post('/email', emailController.insertNew);
 app.get('/email', emailController.findAll);
 
